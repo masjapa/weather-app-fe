0823d23c4ce871c4e6263f4c301f713493c97007
masjapa <ariqdaryprayogo@gmail.com>
2024-06-07 21:22:02 +0700

    initial commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+
+function handleImageError() {
+    document.getElementById('screenshot-container')?.classList.add('!hidden');
+    document.getElementById('docs-card')?.classList.add('!row-span-1');
+    document.getElementById('docs-card-content')?.classList.add('!flex-row');
+    document.getElementById('background')?.classList.add('!hidden');
+}
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <div>
+      <div class="text-white">
+          <div
+              class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800"
+          >
+              <div class="pt-3 sm:pt-5">
+                  <h1>Information</h1>
+              </div>
+          </div>
+      </div>
+    </div>
+</template>
